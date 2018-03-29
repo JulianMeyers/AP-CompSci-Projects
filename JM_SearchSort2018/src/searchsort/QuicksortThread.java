8843db0cf493a4dff1348abb68e88da86bef51c3
Julian Laptop <Julian.Meyers@Kinkaid.Org>
2018-03-29 10:46:01 -0500

    Switched to use different quicksort implementation, added action indicators for pivot, high and low. Now has pivot at middle to prevent worst case performance

--- JM_SearchSort2018/src/searchsort/QuicksortThread.java
+++ JM_SearchSort2018/src/searchsort/QuicksortThread.java
@@ -1,5 +1,7 @@
 package searchsort;
 
+import javax.swing.*;
+
 public class QuicksortThread extends AbstractSearchSortThread {
     public QuicksortThread(BarArray barArray, StatsThread stats, ThreadCompletedDelegate del) {
         super("QuicksortThread", barArray, stats, del);
@@ -8,7 +10,7 @@ public class QuicksortThread extends AbstractSearchSortThread {
     @Override
     public int executeAlgorithm() throws InterruptedException {
         int numBars = mainArray.size();
-        sort(0, numBars-1);
+        quickSort(0, numBars-1);
         return STATUS_FINISHED;
     }
 
@@ -17,7 +19,8 @@ public class QuicksortThread extends AbstractSearchSortThread {
         int i = lowerIndex;
         int j = higherIndex;
         // calculate pivot number, I am taking pivot as middle index number
-        SortableBar pivot = mainArray.get(lowerIndex+(higherIndex-lowerIndex)/2);
+        int pivotPos = lowerIndex+(higherIndex-lowerIndex)/2;
+        SortableBar pivot = mainArray.get(pivotPos);
         // Divide into two arrays
         while (i <= j) {
             /**
@@ -28,6 +31,9 @@ public class QuicksortThread extends AbstractSearchSortThread {
              */
             ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("i", i);
             ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("j", j);
+            ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("h", higherIndex);
+            ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("l", lowerIndex);
+            ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("p",pivotPos);
             while (mainArray.get(i).compareTo(pivot) < 0) {
                 i++;
                 checkIn();
@@ -49,56 +55,6 @@ public class QuicksortThread extends AbstractSearchSortThread {
             quickSort(lowerIndex, j);
         if (i < higherIndex)
             quickSort(i, higherIndex);
-    }
-
-    /* This function takes last element as pivot,
-       places the pivot element at its correct
-       position in sorted array, and places all
-       smaller (smaller than pivot) to left of
-       pivot and all greater elements to right
-       of pivot */
-    int partition(int low, int high) throws InterruptedException {
-        ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("low", low); //lets the graphics know that the "i" variable has changed.
-        ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("high", high); //lets the graphics know that the "i" variable has changed.
-        SortableBar pivot = mainArray.get(high);//was arr.get(high)
-        int i = (low-1); // index of smaller element
-        for (int j=low; j<high; j++)
-        {
-            // If current element is smaller than or
-            // equal to pivot
-            if (mainArray.get(j).compareTo(pivot) <= 0)
-            {
-                i++;
-
-                // swap arr[i] and arr[j]
-                mainArray.swap(i,j);
-            }
-            checkIn();
-        }
-
-        // swap arr[i+1] and arr[high] (or pivot)
-        mainArray.swap(i+1, high);
-
-        return i+1;
-    }
-
-
-    /* The main function that implements QuickSort()
-      arr[] --> Array to be sorted,
-      low  --> Starting index,
-      high  --> Ending index */
-    void sort(int low, int high) throws InterruptedException {
-        if (low < high)
-        {
-            /* pi is partitioning index, arr[pi] is
-              now at right place */
-            int pi = partition(low, high);
-
-            // Recursively sort elements before
-            // partition and after partition
-            sort(low, pi-1);
-            sort(pi+1, high);
-        }
     }
 
 
