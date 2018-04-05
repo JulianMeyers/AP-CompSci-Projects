b903f44e1fe66046f006a8198fdcf64585a7158d
Julian Laptop <Julian.Meyers@Kinkaid.Org>
2018-04-05 10:34:59 -0500

    Added indicators for Shell Sort

--- JM_SearchSort2018/src/searchsort/ShellSortThread.java
+++ JM_SearchSort2018/src/searchsort/ShellSortThread.java
@@ -22,18 +22,21 @@ public class ShellSortThread extends AbstractSearchSortThread {
             // The first gap elements a[0..gap-1] are already
             // in gapped order keep adding one more element
             // until the entire array is gap sorted
+            ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("gap", gap);
             for (int i = gap; i < n; i += 1)
             {
                 // add a[i] to the elements that have been gap
                 // sorted save a[i] in temp and make a hole at
                 // position i
                 SortableBar temp = mainArray.get(i);
+                ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("i", i);
 
                 // shift earlier gap-sorted elements up until
                 // the correct location for a[i] is found
                 int j;
                 for (j = i; j >= gap && mainArray.get(j-gap).compareTo(temp)>0; j -= gap) {
                     mainArray.set(j, mainArray.get(j - gap));
+                    ActionIndicatorQueue.sharedActionIndicatorQueue().addVariable("j", j);
                     checkIn();
                 }
 
